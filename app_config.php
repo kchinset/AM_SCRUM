0a43e07c5a6ffa0cc6555c96dbed0452d1ad6c11
StanislausWibowo <166040698+StanislausWibowo@users.noreply.github.com>
2024-04-16 13:43:29 -0500

    app_config.php

---
+++ app_config.php
@@ -0,0 +1,11 @@
+<?php 
+//application folder name on your web root or htdocs folder
+define('APP_ROOT',$_SERVER['DOCUMENT_ROOT']);
+//DEBUG echo (APP_ROOT)."      <br>";
+define('APP_FOLDER_NAME', '/CIS4033/ScrumProject');
+define('WEB_ROOT','http://'.$_SERVER['SERVER_NAME'].':8080');
+//DEBUG echo(WEB_ROOT);
+define('DSN1', 'mysql:host=localhost;dbname=ACMEMed_SCRUM');
+define('USER1','root');
+define('PASSWD1','');
+?>
