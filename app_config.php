70b7e870fce62420a11a3d4e34b64fd7a4c853a3
NightHawk-real <colton.henkle@gmail.com>
2024-04-17 19:41:45 -0500

    Changed App Folder Name to AM_SCRUM for ease of access

--- app_config.php
+++ app_config.php
@@ -2,7 +2,7 @@
 //application folder name on your web root or htdocs folder
 define('APP_ROOT',$_SERVER['DOCUMENT_ROOT']);
 //DEBUG echo (APP_ROOT)."      <br>";
-define('APP_FOLDER_NAME', '/CIS4033/ScrumProject');
+define('APP_FOLDER_NAME', '/CIS4033/AM_SCRUM');
 define('WEB_ROOT','http://'.$_SERVER['SERVER_NAME'].':8080');
 //DEBUG echo(WEB_ROOT);
 define('DSN1', 'mysql:host=localhost;dbname=ACMEMed_SCRUM');
